4eaedc0b957a8f40e96d0d3bcf3452cbf262dc24
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-05-10 23:41:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/so-yummy-project">
+        <BrowserRouter basename="/react_node_fs_58_KateDietkova">
           <App />
         </BrowserRouter>
       </PersistGate>
